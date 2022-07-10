3f6c95c1c98e540f587ba95bb39b9019fe1f25a2
m.naumann <m.naumann@windot.de>
2022-07-10 22:59:14 +0200

    add dark mode detection

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,41 @@ import ButtonLeft from './components/ButtonLeft/ButtonLeft';
 import ButtonRight from './components/ButtonRight/ButtonRight';
 
 function App() {
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    document.documentElement.classList.add('dark');
+  }
+
+  const buttonClickHandler = (direction: string, part: string) => {
+    console.log(direction, part);
+  };
+
   return (
-    <main>
-      <section>
-        <div id='left-button-wrapper'>
-          <ButtonLeft />
-          <ButtonLeft />
-          <ButtonLeft />
-          <ButtonLeft />
-          <ButtonLeft />
-        </div>
-        <canvas></canvas>
-        <div id='right-button-wrapper'>
-          <ButtonRight />
-          <ButtonRight />
-          <ButtonRight />
-          <ButtonRight />
-          <ButtonRight />
-        </div>
-      </section>
-    </main>
+    <>
+      <header />
+      <main>
+        <section>
+          <div id='left-button-wrapper'>
+            <ButtonLeft />
+            <ButtonLeft />
+            <ButtonLeft />
+            <ButtonLeft />
+            <ButtonLeft />
+          </div>
+          <canvas></canvas>
+          <div id='right-button-wrapper'>
+            <ButtonRight />
+            <ButtonRight />
+            <ButtonRight />
+            <ButtonRight />
+            <ButtonRight />
+          </div>
+        </section>
+      </main>
+      <footer />
+    </>
   );
 }
 
