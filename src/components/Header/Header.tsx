469d45e1420d7f067509611eaa754c85ef84362d
m.naumann <m.naumann@windot.de>
2022-07-10 22:58:47 +0200

    add dark mode detection

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.scss';
+export default function Header() {
+  return (
+    <header>
+      <section></section>
+      <section></section>
+      <section></section>
+    </header>
+  );
+}
